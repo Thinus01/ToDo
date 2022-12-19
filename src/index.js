0d2c03d97b0554eef11dabf4a1938c3e422c0714
Thinus <thinusvdv2001@gmail.com>
2022-12-19 13:18:25 +0200

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,2 @@
 import './style.css';
 import './modules/add.js';
-
-// import './style.css';
-// import refresh from './assets/refresh.png';
-// import { add } from './modules/add.js'
-
-// const div = document.querySelector('#list');
-// const todo = document.querySelector('#ToDo');
-
-// //arrVal = imported list array
-// const arrVal = new add();
-
-
-// const refreshIcon = new Image();
-// refreshIcon.src = refresh;
-// todo.appendChild(refreshIcon);
--- src/modules/edit.js
+++
@@ -1,7 +0,0 @@
-import trigger from './add.js'
-
-const triggerer = new trigger();
-
-function edit() {
-  console.log('hi');
-}
